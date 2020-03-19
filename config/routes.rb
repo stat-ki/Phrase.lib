25fd582fd83b77b5460a9f3df061ffad41dd33d0
stat-ki <per_ik@outlook.com>
2020-03-19 10:56:11 +0000

    #7 Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # home_controller
+  root "home#top"
+
+  # devise_controller
+  devise_for :users
+  # users_controller
+  resources :users, only: [:show]
+
+  # posts_controller
+  resources :posts, except: [:index]
+  get "/notes" => "posts#notes"
+  get "/shares" => "posts#shares"
+  get "/first_post" => "posts#first_post"
+
+  # favorites_controller
+  post "/posts/:post_id/favorite" => "favorites#create"
+  delete "/posts/:post_id/favorite" => "favorites#destroy"
+
+  # search_controller
+  post "/search_result" => "search#search"
+
 end
