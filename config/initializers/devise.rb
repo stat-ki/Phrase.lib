58fd87a486a88c322d79aa3a342a6a51a6d1645f
stat-ki <per_ik@outlook.com>
2020-03-29 02:04:44 +0000

    #91 Change devise controller & config to enable to Google authenticate

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  require 'devise/orm/active_record'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -261,6 +262,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_SERCRET_KEY']
+  config.omniauth :google_oauth2, ENV['GOOGLE_API_KEY'], ENV['GOOGLE_SERCRET_KEY']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
