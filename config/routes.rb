d4e0627e60e6506a6077c6f13670d5bcbaf51080
stat-ki <per_ik@outlook.com>
2020-03-30 05:00:11 +0000

    #101 Add yml files to localize error messages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # devise_controller
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
