fbd97d7a1ad778a74851eba27097c57ce59f8773
stat-ki <per_ik@outlook.com>
2020-04-11 06:55:25 +0000

    Create post model test & modify validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites
 
+  validates :source_id, presence: true, unless: :is_original
   validates :phrase, presence: true, length: { maximum: 50 }
+  validates :language, presence: true
+  validates :is_original, presence: true
+  validates :is_sharing, presence: true
 
   enum language: { :ja => 0, :en => 1, :fr => 2, :it => 3, :zh => 4, :other => 5 }
 
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     sequence(:phrase) { "This is a test." }
     sequence(:language) { 1 }
     sequence(:detail) { "detail" }
+    sequence(:is_original) { true }
+    sequence(:is_sharing) { true }
   end
 end
